7d0a3baf4969d0f02fe8a8c69acb8a055341f029
O. Ielfimov <oleksii.ielfimov@gmail.com>
2012-09-08 13:24:24 +0300

    'first'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeMoney::Application.config.secret_token = '085ca53a5354efe594b835fc358b423b89435ffa03f5894671ce760126cc81456144c6864bcef18b423e88e0bc070d0f94c3c3eedb1eea815b4d7cff88be22cc'
