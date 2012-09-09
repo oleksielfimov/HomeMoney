ba4838bd798b0982f9777ee33f31f10bfbf2053c
O. Ielfimov <oleksii.ielfimov@gmail.com>
2012-09-09 20:59:27 +0300

    'erroradd'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
-module ApplicationHelper
+﻿module ApplicationHelper
+def title
+	base_title = "HomeMoney"
+	if @title.nil?
+	base_title
+	else
+	" #{base_title} | #{@title} "
+	end
 end
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 HomeMoney::Application.routes.draw do
+  get "redirect/error"
+
+  get "pages/home"
+
+  get "pages/buy"
+
+  get "pages/account"
+
+  get "pages/report"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +58,8 @@ HomeMoney::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
+   
 
   # See how all your routes lay out with "rake routes"
 
